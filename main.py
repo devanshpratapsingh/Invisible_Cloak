bd735bc4d86e32f358af4357e2522140e05a1eeb
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-07 21:47:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import time
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #you can change this according to your choice of camera, my initial webcam is 0 
 
 time.sleep(3)
 
